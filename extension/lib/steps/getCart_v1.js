7de55fb15dcce4269a36b8cf4b07453f7c113d50
Aaron Martin <aaron.martin@shopgate.com>
2018-01-08 12:48:23 -0600

    GENERAL-STRUCTURE: added ShopgateCartIdRepository

--- extension/lib/steps/getCart_v1.js
+++ extension/lib/steps/getCart_v1.js
@@ -1,38 +1,37 @@
 const ShopgateCartRepository = require('../cart/ShopgateCartRepository')
 const BigCommerceFactory = require('./BigCommerceFactory.js')
+const ShopgateCartIdRepository = require('../cart/ShopgateCartIdRepository')
 
 /**
  * @param {Object} context
  * @param {GetProductDescriptionInput} input
  * @param {GetCartCallback} cb
  */
-module.exports = function (context, input, cb) {
+module.exports = async (context, input, cb) => {
   const bigCommerceFactory = new BigCommerceFactory(
     context.config.clientId,
     context.config.accessToken,
     context.config.storeHash)
-  context.log.debug('client id: ' + context.config.clientId)
   const shopgateCartRepository = new ShopgateCartRepository(bigCommerceFactory.createV3())
-
-  context.storage.device.get('cartId', function (err, cartId) {
-    context.log.debug('HEY AARON, I am trying to get the cartId from storage. This is what I got: ' + cartId)
-    if (err || !cartId) {
-      context.log.debug('HEY AARON, I am using createAndGetCart')
-      createAndGetCart(shopgateCartRepository, context, cb)
-    } else {
-      context.log.debug('HEY AARON, I am using getCart')
-      getCart(cartId, shopgateCartRepository, context, cb)
-    }
-  })
+  const shopgateCartIdRepository = new ShopgateCartIdRepository(context)
+  try {
+    const cartId = await shopgateCartIdRepository.get()
+    context.log.debug('Found this cartId: ' + cartId)
+    getCart(cartId, shopgateCartRepository, shopgateCartIdRepository, context, cb)
+  } catch (error) {
+    context.log.debug('Unable to find cartId. Calling createAndGetCart')
+    createAndGetCart(shopgateCartRepository, shopgateCartIdRepository, context, cb)
+  }
 }
 
 /**
  * @param {string} cartId
  * @param {ShopgateCartRepository} shopgateCartRepository
+ * @param {ShopgateCartIdRepository} shopgateCartIdRepository
  * @param {object} context
  * @param {GetCartCallback} cb
  */
-const getCart = async (cartId, shopgateCartRepository, context, cb) => {
+const getCart = async (cartId, shopgateCartRepository, shopgateCartIdRepository, context, cb) => {
   try {
     const cart = await shopgateCartRepository.get(cartId)
     context.log.debug(JSON.stringify(cart))
@@ -40,7 +39,7 @@ const getCart = async (cartId, shopgateCartRepository, context, cb) => {
   } catch (error) {
     context.log.error('get cart error object: ' + JSON.stringify(error))
     if (error.code === 404) {
-      createAndGetCart(shopgateCartRepository, context, cb)
+      createAndGetCart(shopgateCartRepository, shopgateCartIdRepository, context, cb)
     } else {
       context.log.error('Unable to get shopping cart for cartId: ' + cartId)
       cb(error)
@@ -50,22 +49,18 @@ const getCart = async (cartId, shopgateCartRepository, context, cb) => {
 
 /**
  * @param {ShopgateCartRepository} shopgateCartRepository
+ * @param {ShopgateCartIdRepository} shopgateCartIdRepository
  * @param {object} context
  * @param {GetCartCallback} cb
  */
 
-const createAndGetCart = async (shopgateCartRepository, context, cb) => {
+const createAndGetCart = async (shopgateCartRepository, shopgateCartIdRepository, context, cb) => {
   try {
     const cartResponse = await shopgateCartRepository.createAndGet()
     context.log.error(JSON.stringify(cartResponse))
-    context.storage.device.set('cartId', cartResponse.id, function (err) {
-      if (err) {
-        context.log.error('Could not store cart id in device storage')
-        cb(err)
-      }
-      context.log.debug('created and got cart: ' + JSON.stringify(cartResponse.cart))
-      cb(null, cartResponse.cart)
-    })
+    const cartId = await shopgateCartIdRepository.set(cartResponse.id)
+    context.log.debug('a cart was created as part of a getCart. It has the id of: ' + cartId)
+    cb(null, cartResponse.cart)
   } catch (error) {
     context.log.error('Unable to create and get shopping cart for cartId:')
     cb(error)
