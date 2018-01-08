49703d54d2d35786d52e67a197671f949938e2f8
Aaron Martin <aaron.martin@shopgate.com>
2018-01-08 13:10:44 -0600

    GENERAL-STRUCTURE: cleaned up getCart_v1.js

--- extension/lib/steps/getCart_v1.js
+++ extension/lib/steps/getCart_v1.js
@@ -17,52 +17,17 @@ module.exports = async (context, input, cb) => {
   try {
     const cartId = await shopgateCartIdRepository.get()
     context.log.debug('Found this cartId: ' + cartId)
-    getCart(cartId, shopgateCartRepository, shopgateCartIdRepository, context, cb)
-  } catch (error) {
-    context.log.debug('Unable to find cartId. Calling createAndGetCart')
-    createAndGetCart(shopgateCartRepository, shopgateCartIdRepository, context, cb)
-  }
-}
-
-/**
- * @param {string} cartId
- * @param {ShopgateCartRepository} shopgateCartRepository
- * @param {ShopgateCartIdRepository} shopgateCartIdRepository
- * @param {object} context
- * @param {GetCartCallback} cb
- */
-const getCart = async (cartId, shopgateCartRepository, shopgateCartIdRepository, context, cb) => {
-  try {
     const cart = await shopgateCartRepository.get(cartId)
-    context.log.debug(JSON.stringify(cart))
     cb(null, cart)
   } catch (error) {
-    context.log.error('get cart error object: ' + JSON.stringify(error))
-    if (error.code === 404) {
-      createAndGetCart(shopgateCartRepository, shopgateCartIdRepository, context, cb)
-    } else {
-      context.log.error('Unable to get shopping cart for cartId: ' + cartId)
+    try {
+      context.log.debug('An error occurred while trying to get the cart. Will try createAndGet')
+      const cartResponse = await shopgateCartRepository.createAndGet()
+      const cartId = await shopgateCartIdRepository.set(cartResponse.id)
+      context.log.debug('a cart was created as part of a getCart. It has the id of: ' + cartId)
+      cb(null, cartResponse.cart)
+    } catch (error) {
       cb(error)
     }
-  }
-}
-
-/**
- * @param {ShopgateCartRepository} shopgateCartRepository
- * @param {ShopgateCartIdRepository} shopgateCartIdRepository
- * @param {object} context
- * @param {GetCartCallback} cb
- */
-
-const createAndGetCart = async (shopgateCartRepository, shopgateCartIdRepository, context, cb) => {
-  try {
-    const cartResponse = await shopgateCartRepository.createAndGet()
-    context.log.error(JSON.stringify(cartResponse))
-    const cartId = await shopgateCartIdRepository.set(cartResponse.id)
-    context.log.debug('a cart was created as part of a getCart. It has the id of: ' + cartId)
-    cb(null, cartResponse.cart)
-  } catch (error) {
-    context.log.error('Unable to create and get shopping cart for cartId:')
-    cb(error)
   }
 }
