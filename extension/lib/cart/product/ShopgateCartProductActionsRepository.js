d9a616c6ebdb8997200629fe0fe8a6ef75b47b25
Aaron Martin <aaron.martin@shopgate.com>
2018-01-09 10:03:43 -0600

    BIGC-548: Removed unneeded file and modified ShopgateCartProductActionsRepository to only handle adding product to cart

--- extension/lib/cart/product/ShopgateCartProductActionsRepository.js
+++ extension/lib/cart/product/ShopgateCartProductActionsRepository.js
@@ -1,5 +1,4 @@
 const BigCommerceLineItemsBuilder = require('./BigCommerceLineItemsBuilder')
-const BigcommerceUpdateObjectsBuilder = require('./BigcommerceUpdateObjectsBuilder')
 
 class ShopgateCartProductActionsRepository {
   /**
@@ -31,36 +30,6 @@ class ShopgateCartProductActionsRepository {
     const bigcommerceCartAddProductResponse = await this._apiVersion3Client.post('/carts/', {'line_items': bigCommerceLineItemsBuilder.build()})
 
     return {cartId: bigcommerceCartAddProductResponse.data.id, 'response': bigcommerceCartAddProductResponse}
-  }
-
-  /**
-   * @param {array} cartItemIds
-   * @param {string} cartId
-   * @return {Promise.<object>}
-   */
-  async removeProductsFromCart (cartItemIds, cartId) {
-    const productRemovalPromises = []
-    for (const cartItemId of cartItemIds) {
-      productRemovalPromises.push(this._apiVersion3Client.delete('/carts/' + cartId + '/items/' + cartItemId))
-    }
-
-    return Promise.all(productRemovalPromises)
-  }
-
-  /**
-   *
-   * @param {string} cartId
-   * @param {ShopgateCart} currentCart
-   * @param {object[]} smallCartItems
-   * @return {Promise.<*>}
-   */
-  async updateProductInCart (cartId, currentCart, smallCartItems) {
-    const bigcommerceProductUpdatesBuilder = new BigcommerceUpdateObjectsBuilder(currentCart, smallCartItems)
-    const productUpdatePromises = []
-    for (const bigCommerceUpdate of bigcommerceProductUpdatesBuilder.build()) {
-      productUpdatePromises.push(this._apiVersion3Client.put('/carts/' + cartId + '/items/' + bigCommerceUpdate.cartItemId, {'cart_id': cartId, 'item_id': bigCommerceUpdate.cartItemId, 'line_item': bigCommerceUpdate.lineItem}))
-    }
-    return Promise.all(productUpdatePromises)
   }
 }
 
