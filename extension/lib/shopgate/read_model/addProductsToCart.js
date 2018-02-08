2d5f07b039c445e7cec5b8099e152ad2e2421d12
Aaron Martin <aaron.martin@shopgate.com>
2018-02-08 09:58:45 -0600

    BIGC-677: adjustment to merge master back into branch

--- extension/lib/shopgate/read_model/addProductsToCart.js
+++ extension/lib/shopgate/read_model/addProductsToCart.js
@@ -1,20 +1,3 @@
 /**
- * @callback AddProductsToCartCallback
- * @param {Error} [error]
- * @param {ShopgateAddProductResponse} [AddProductResponse]
- */
-
-/**
- *  @typedef {Object} ShopgateAddProductResponse
- */
-
-/**
- *  @typedef {Object} AddProductsInput
- *  @property {ShopgateAddProduct[]} products
- */
-
-/**
- * @typedef {Object} ShopgateAddProduct
- * @property {string} productId
- * @property {number} quantity
+ *  @typedef {Object} AddProductsToCartResponse
  */
