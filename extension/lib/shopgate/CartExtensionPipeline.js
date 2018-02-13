2f485d634c43d4cf2545cb9c44a9a462c2faf2f6
Aaron Martin <aaron.martin@shopgate.com>
2018-02-13 11:43:09 -0600

    BIGC-636: corrected JSDoc

--- extension/lib/shopgate/CartExtensionPipeline.js
+++ extension/lib/shopgate/CartExtensionPipeline.js
@@ -89,7 +89,7 @@ class ShopgateCartExtensionPipeline {
   }
 
   /**
-   * @param {[string]} cartItems
+   * @param {string[]} cartItems
    * @return {Promise<boolean>}
    */
   async updateProducts (cartItems) {
