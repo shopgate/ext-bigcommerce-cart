11bb484c1a51b2f39d2aa977c1e7045e58ebee64
Christian Frenzl <christian.frenzl@shopgate.com>
2018-04-11 18:05:53 +0200

    BIGC-668 unit test

---
+++ test/unit/extension/lib/mergeCartsOnLoginTest.js
@@ -0,0 +1,117 @@
+'use strict'
+const sinon = require('sinon')
+
+const ShopgateCartPipeline = require('../../../../extension/lib/shopgate/CartExtensionPipeline')
+const mergeCartsOnLogin = require('../../../../extension/lib/mergeCartsOnLogin')
+
+describe('mergeCartsOnLogin', () => {
+  it('should put all items from the anonymous cart into the user cart', async () => {
+    let getCartResponse = {
+      isOrderable: true,
+      isTaxIncluded: false,
+      currency: 'USD',
+      messages: [],
+      text: [],
+      cartItems: [
+        {
+          id: '95d7e6e1-8787-4e1a-9252-c55cb4c5c19b',
+          quantity: 1,
+          type: 'product',
+          coupon: {},
+          product: {
+            id: 120,
+            name: 'Dalmatian',
+            additionalInfo: [],
+            featuredImageUrl: 'https://cdn7.bigcommerce.com/s-r5s844ad/products/120/images/364/dalmatian-puppy-care__25239.1513768807.195.195.png?c=2',
+            properties: [],
+            price: {
+              unit: 100,
+              default: 100,
+              special: null
+            },
+            appliedDiscounts: []
+          },
+          messages: []
+        },
+        {
+          id: '28c02f8b-a0ab-4c5a-aef7-fd27eec6c358',
+          quantity: 1,
+          type: 'product',
+          coupon: {},
+          product: {
+            id: 121,
+            name: 'German Shepherd',
+            additionalInfo: [],
+            featuredImageUrl: 'https://cdn7.bigcommerce.com/s-r5s844ad/products/121/images/365/87788187__37415.1513768875.195.195.jpg?c=2',
+            properties: [],
+            price: {
+              unit: 500,
+              default: 500,
+              special: null
+            },
+            appliedDiscounts: []
+          },
+          messages: []
+        }
+      ],
+      totals: [
+        {
+          type: 'discount',
+          label: 'Discount',
+          amount: 7.75,
+          subTotals: []
+        },
+        {
+          type: 'grandTotal',
+          label: 'Total',
+          amount: 592.25,
+          subTotals: []
+        },
+        {
+          type: 'subTotal',
+          label: 'SubTotal',
+          amount: 600,
+          subTotals: []
+        }
+      ],
+      flags: {
+        taxIncluded: false,
+        orderable: true,
+        coupons: false
+      }
+    }
+
+    const pipelineStub = sinon.createStubInstance(ShopgateCartPipeline)
+    sinon.stub(ShopgateCartPipeline, 'createForDevice').returns(pipelineStub)
+    sinon.stub(ShopgateCartPipeline, 'create').returns(pipelineStub)
+    pipelineStub.get.returns(getCartResponse)
+
+    const context = {
+      meta: {
+        deviceId: '9729bcf398c10598aa23a04b3557a4c750c714eb5e177375196660beeb506704',
+        userId: '15358639',
+        appId: 'shop_31422'
+      },
+      storage: {
+        extension: null,
+        device: null,
+        user: null
+      },
+      config: {
+        storeHash: null,
+        accessToken: null,
+        clientId: null,
+        clientSecret: null,
+        requestTimeout: '15000'
+      }
+    }
+    await mergeCartsOnLogin(context, {})
+
+    sinon.assert.calledWith(pipelineStub.addProducts, [
+      {productId: 120, quantity: 1},
+      {productId: 121, quantity: 1}
+    ])
+
+    sinon.assert.calledOnce(pipelineStub.destroyCart)
+  })
+})
