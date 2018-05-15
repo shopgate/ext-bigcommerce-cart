1b2bc6f83e619bd5503b06d775c5b206b63bbc28
O.Bilenko <alexbridge.de@gmail.com>
2018-05-15 09:05:02 +0200

    PWA-152, PWA-345. Add frontend subscription for opening checkout page (#19)
    * PWA-152, PWA-345. Add frontend subscription for opening checkout page
    
    * Added a LF to the end of the file
    
    * Delete .npmrc
    
    * Update .gitignore

---
+++ frontend/Checkout/subscribers.js
@@ -0,0 +1,40 @@
+import goBackHistory from '@shopgate/pwa-common/actions/history/goBackHistory';
+import { isUserLoggedIn } from '@shopgate/pwa-common/selectors/user';
+import ParsedLink from '@shopgate/pwa-common/components/Router/helpers/parsed-link';
+import trackingCore from '@shopgate/tracking-core/core/Core';
+import { LEGACY_URL } from '@shopgate/pwa-common-commerce/checkout/constants';
+import { openedCheckoutLink$ } from '@shopgate/pwa-common-commerce/checkout/streams';
+import fetchCheckoutUrl from '@shopgate/pwa-common-commerce/checkout/actions/fetchCheckoutUrl';
+
+/**
+ * Checkout subscriptions.
+ * @param {Function} subscribe The subscribe function.
+ */
+export default function checkout(subscribe) {
+  /**
+   * Gets triggered when the user enters the checkout.
+   */
+  subscribe(openedCheckoutLink$, ({ dispatch, getState }) => {
+    // Check if user is logged in.
+    if (!isUserLoggedIn(getState())) {
+      return;
+    }
+
+    dispatch(fetchCheckoutUrl())
+      .then((url) => {
+        /**
+         * Build the complete checkout url. Fallback to the
+         * legacy url if the Pipeline returns an invalid url.
+         * Add some tracking params for cross domain tracking.
+         */
+        const checkoutUrl = trackingCore.crossDomainTracking(url || LEGACY_URL);
+
+        // Open the checkout.
+        const link = new ParsedLink(checkoutUrl);
+        link.open();
+      })
+      .catch(e => e);
+
+    dispatch(goBackHistory(1));
+  });
+}
