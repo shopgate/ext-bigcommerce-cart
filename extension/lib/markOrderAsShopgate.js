41816c534bfc546dcd60ce3feecbded9da9f6673
Christian Frenzl <christian.frenzl@shopgate.com>
2018-05-24 16:38:55 +0200

    BIGC-726 getProductIdsFromCache doesn't fail anymore if it can't read from extension storage

---
+++ extension/lib/markOrderAsShopgate.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const BigCommerceFactory = require('./bigcommerce/Factory')
+
+/**
+ * @param {PipelineContext} context
+ * @param {Object} input
+ * @returns {Promise<void>}
+ */
+module.exports = async (context, input) => {
+  try {
+    const apiClientV2 = BigCommerceFactory.createV2(
+      context.config.clientId,
+      context.config.accessToken,
+      context.config.storeHash
+    )
+
+    await apiClientV2.put(`/orders/${input.orderId}`, {
+      staff_notes: 'Order placed via Shopgate App'
+    })
+  } catch (error) {
+    context.log.error(error.message)
+    throw error
+  }
+}
