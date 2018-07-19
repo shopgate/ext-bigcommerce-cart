05fe5e6551ef26de3ac1e938930e67d0a317a492
Szymon Nowicki <sznowicki@eskalacja.com>
2018-07-19 10:32:18 +0200

    PWA-487 - getCheckoutUrl timeout as a constant

--- frontend/Checkout/subscribers.js
+++ frontend/Checkout/subscribers.js
@@ -1,7 +1,10 @@
 import goBackHistory from '@shopgate/pwa-common/actions/history/goBackHistory';
 import ParsedLink from '@shopgate/pwa-common/components/Router/helpers/parsed-link';
 import trackingCore from '@shopgate/tracking-core/core/Core';
-import { LEGACY_URL } from '@shopgate/pwa-common-commerce/checkout/constants';
+import {
+  LEGACY_URL,
+  FETCH_CHECKOUT_URL_TIMEOUT,
+} from '@shopgate/pwa-common-commerce/checkout/constants';
 import { openedCheckoutLink$ } from '@shopgate/pwa-common-commerce/checkout/streams';
 import fetchCheckoutUrl from '@shopgate/pwa-common-commerce/checkout/actions/fetchCheckoutUrl';
 import { appDidStart$ } from '@shopgate/pwa-common/streams/app';
@@ -26,8 +29,8 @@ export default function checkout(subscribe) {
     const started = Date.now();
     dispatch(fetchCheckoutUrl())
       .then((url) => {
-        // Forget if it took more than 20 seconds. User is already back.
-        if (Date.now() - started > 20000) {
+        // Forget if it took more than PWA allows. User is already back.
+        if (Date.now() - started > FETCH_CHECKOUT_URL_TIMEOUT) {
           return;
         }
         /**
